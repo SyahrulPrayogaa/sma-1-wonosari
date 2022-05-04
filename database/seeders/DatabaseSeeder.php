1614f27d692c75a1467f727f284c220b60f327a8
SyahrulPrayogaa <syahrulprayoga@gmail.com>
2022-05-04 16:06:22 +0800

    disable siswa seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(MataPelajaranTableSeeder::class);
-        $this->call(SiswaTableSeeder::class);
+        // $this->call(SiswaTableSeeder::class);
     }
 }
